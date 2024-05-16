75dd5816a3f0a8c760821a8ce2df2e4625d93d4b
Your Name <you@example.com>
2024-05-16 20:04:00 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -64,7 +64,7 @@ List* get_adj_nodes(Node* n){
             if(n->sudo[i][j]==0)
             {
                 Node* adj=copy(n);
-                adj->sudo[i][j]=0;
+                adj->sudo[i][j]=2;
                 pushBack(list,adj);
             }
         }
